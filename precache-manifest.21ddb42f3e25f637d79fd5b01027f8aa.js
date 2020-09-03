0198388fa7e2a76daab9e46ea99d189358a30e6d
Julia <julia.woronkow@gmail.com>
2020-09-03 17:25:02 +0200

    Updates

---
+++ precache-manifest.21ddb42f3e25f637d79fd5b01027f8aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03734547bfa0cd394bcc2bbac627b0f2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de0fc7d27da190f3289c",
+    "url": "./static/css/main.71022d22.chunk.css"
+  },
+  {
+    "revision": "305fc2a2a037f273372a",
+    "url": "./static/js/2.cd39d232.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.cd39d232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0fc7d27da190f3289c",
+    "url": "./static/js/main.b87c6057.chunk.js"
+  },
+  {
+    "revision": "29c2e50c4a8e8303d37b",
+    "url": "./static/js/runtime-main.56c91fb7.js"
+  },
+  {
+    "revision": "19113a796272b537b943f8de907d9ce9",
+    "url": "./static/media/karli.19113a79.png"
+  }
+]);
